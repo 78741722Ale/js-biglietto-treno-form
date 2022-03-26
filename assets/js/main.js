ce368144ee1771bec43777dd72595d02353202f9
78741722Ale <pecorillaalessandro@gmail.com>
2022-03-26 17:54:18 +0100

    Aggiunte Funzionalità di JS e modifiche al foglio di stile

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,8 +9,12 @@
 // va applicato uno sconto del 40% per gli over 65.
 
 /// Primo problema - chiedi all'utente il nome e cognome
-const userName = prompt("Inserisci nome e cognome");
-// Fallo uscire in console Log
+// Nome e cognome
+const first_name = prompt("Inserisci il tuo nome");
+const last_name = prompt("Inserisci il tuo cognome");
+console.log(first_name,last_name);
+// Nome Completo
+const userName = first_name + ' ' + last_name;
 console.log(userName);
 // Dichiarazione nel form
 document.getElementById("name").value = userName;
@@ -27,6 +31,67 @@ if (isNaN(`${userKilometers}`)) {
     // Dichiarazione nel form
     document.getElementById("kilometers").value = userKilometers;
 }
-
-
 ///
+
+// Sulla base di queste informazioni calcola :
+// costanti fisse - prezzo al km
+let price_for_km = 0.21;
+console.log(price_for_km);
+/// prezzo totale del viaggio in base ai KM, per persone tra i 18 anni e i 64
+let price_adults = (price_for_km * userKilometers);
+
+
+// variabile - età dichiarate
+const under_age = "Minorenne";
+console.log(Minorenne);
+document.getElementById("Minorenne").innerHTML = under_age;
+// 
+const over_age = "Anziano";
+console.log(Anziano);
+document.getElementById("Anziano").innerHTML = over_age;
+
+
+// Sconto per Under 18
+const sales_under_age = 0.8;
+const sales_over_age = 0.6;
+
+
+/// Dettagli Biglietto
+
+// Carrozza 
+const numero_carrozza = Math.floor(Math.random() * 9) + 1;
+console.log(`Il numero della carrozza è ${numero_carrozza}`);
+
+// CPCode
+const numero_cpcode = Number("97928");
+console.log(`Il numero del codice QR ${numero_cpcode}`);
+
+/// Dettagli del biglietto, al click del "genera"
+function ticket_name() {
+    document.getElementById("name").value = userName;
+    document.getElementById("kilometers").value = userKilometers; 
+    document.getElementById("carrozza").value = numero_carrozza;
+    document.getElementById("CPCode").value = numero_cpcode;
+    document.getElementById("first_name").value = first_name;
+    document.getElementById("last_name").value = last_name;
+}
+
+/// Dettagli del rimossi al click dell'annulla
+function no_details() {
+    document.getElementById("name").value = "";
+    document.getElementById("kilometers").value = "";
+    document.getElementById("carrozza").value = "";
+    document.getElementById("CPCode").value = "";
+    document.getElementById("first_name").value = "";
+    document.getElementById("last_name").value = "";
+}
+
+
+
+
+
+
+
+
+
+
