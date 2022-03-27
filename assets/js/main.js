10e1dd845f5a6faae1ab66ed667e9d52f84f2f71
78741722Ale <pecorillaalessandro@gmail.com>
2022-03-27 14:57:29 +0200

    Esercizio Consegnato non completo, layout pronto ma fermo con JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,11 +8,12 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 
+
 /// Primo problema - chiedi all'utente il nome e cognome
 // Nome e cognome
 const first_name = prompt("Inserisci il tuo nome");
 const last_name = prompt("Inserisci il tuo cognome");
-console.log(first_name,last_name);
+console.log(first_name, last_name);
 // Nome Completo
 const userName = first_name + ' ' + last_name;
 console.log(userName);
@@ -33,32 +34,33 @@ if (isNaN(`${userKilometers}`)) {
 }
 ///
 
-// Sulla base di queste informazioni calcola :
+// Stato ed età passeggeri , variabili 
+userAge = document.getElementById('userAge').value;
+console.log(userAge);
 // costanti fisse - prezzo al km
 let price_for_km = 0.21;
 console.log(price_for_km);
-/// prezzo totale del viaggio in base ai KM, per persone tra i 18 anni e i 64
-let price_adults = (price_for_km * userKilometers);
 
-// Variabili sconti
+// Variabile per dimostrare dettaglio offerta
+let message;
+// visualizzazione dell'offerta al genera 
+document.getElementById('offerta').innerHTML = message;
+
+/// prezzo totale del viaggio in base ai KM, per persone tra i 18 anni e i 64 (adulti)
+let costo_totale = price_for_km * userKilometers;
+console.log(costo_totale);
+// Sconti destinati ad età minorenne e over 65
 const sales_under_age = 0.8;
 const sales_over_age = 0.6;
 
 //
-const Minore = "Minorenne"
-let minorenne = (price_adults * sales_under_age);
-console.log(minorenne);
-document.getElementById("minorenne").innerHTML = Minore;
-//
-const Adulto = "Adulto"
-let adults = price_adults;
-console.log(adults);
-document.getElementById("adulto").innerHTML = Adulto;
-// 
-const Anziano = "Anziano"
-let anziano = (price_adults * sales_over_age);
-console.log(anziano);
-document.getElementById("anziano").innerHTML = Anziano;
+if (userAge == 'Minorenne') {
+    costo_totale = costo_totale * sales_under_age;
+    message = 'Minorenne';
+} else if (userAge == 'Anziano') {
+    costo_totale = costo_totale * sales_over_age;
+    message = 'Anziano';
+};
 
 /// Dettagli Biglietto
 
@@ -71,24 +73,30 @@ const numero_cpcode = Number("97928");
 console.log(`Il numero del codice QR ${numero_cpcode}`);
 
 /// Dettagli del biglietto, al click del "genera"
-function ticket_name() {
+document.getElementById("genera").addEventListener("click", function () {
+    document.getElementById("offerta").innerHTML = message;
     document.getElementById("name").value = userName;
     document.getElementById("kilometers").value = userKilometers;
     document.getElementById("carrozza").value = numero_carrozza;
     document.getElementById("CPCode").value = numero_cpcode;
     document.getElementById("first_name").value = first_name;
     document.getElementById("last_name").value = last_name;
-}
+    document.getElementById("costo_totale").value = costo_totale + '€';
+});
 
-/// Dettagli del rimossi al click dell'annulla
-function no_details() {
-    document.getElementById("name").value = "";
-    document.getElementById("kilometers").value = "";
-    document.getElementById("carrozza").value = "";
-    document.getElementById("CPCode").value = "";
-    document.getElementById("first_name").value = "";
-    document.getElementById("last_name").value = "";
-}
+/// Dettagli rimossi al click dell'annulla
+document.getElementById("annulla").addEventListener("click", function () {
+    document.getElementById("offerta").value = ' ';
+    document.getElementById("name").value = ' ';
+    document.getElementById("kilometers").value = ' ';
+    document.getElementById("carrozza").value = ' ';
+    document.getElementById("CPCode").value = ' ';
+    document.getElementById("first_name").value = ' ';
+    document.getElementById("last_name").value = ' ';
+    document.getElementById("costo_totale").value = ' ';
+});
+
+
 
 
 
